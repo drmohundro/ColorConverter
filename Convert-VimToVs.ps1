0e18a72139d516e0948a10e39ba6fe98af8aa84c
David Mohundro <drmohundro@gmail.com>
2010-03-23 12:24:33 -0500

    Refactored to use a local vssettings file for base.

--- Convert-VimToVs.ps1
+++ Convert-VimToVs.ps1
@@ -2,6 +2,58 @@ param (
     [string] $pathToVim
 )
 
+<#
+--- from http://vimdoc.sourceforge.net/htmldoc/syntax.html#{group-name} ---
+--- see also http://vimdoc.sourceforge.net/htmldoc/syntax.html#highlight-default ---
+
+	*Comment	any comment
+
+	*Constant	any constant
+	 String		a string constant: "this is a string"
+	 Character	a character constant: 'c', '\n'
+	 Number		a number constant: 234, 0xff
+	 Boolean	a boolean constant: TRUE, false
+	 Float		a floating point constant: 2.3e10
+
+	*Identifier	any variable name
+	 Function	function name (also: methods for classes)
+
+	*Statement	any statement
+	 Conditional	if, then, else, endif, switch, etc.
+	 Repeat		for, do, while, etc.
+	 Label		case, default, etc.
+	 Operator	"sizeof", "+", "*", etc.
+	 Keyword	any other keyword
+	 Exception	try, catch, throw
+
+	*PreProc	generic Preprocessor
+	 Include	preprocessor #include
+	 Define		preprocessor #define
+	 Macro		same as Define
+	 PreCondit	preprocessor #if, #else, #endif, etc.
+
+	*Type		int, long, char, etc.
+	 StorageClass	static, register, volatile, etc.
+	 Structure	struct, union, enum, etc.
+	 Typedef	A typedef
+
+	*Special	any special symbol
+	 SpecialChar	special character in a constant
+	 Tag		you can use CTRL-] on this
+	 Delimiter	character that needs attention
+	 SpecialComment	special things inside a comment
+	 Debug		debugging statements
+
+	*Underlined	text that stands out, HTML links
+
+	*Ignore		left blank, hidden
+
+	*Error		any erroneous construct
+
+	*Todo		anything that needs extra attention; mostly the
+			keywords TODO FIXME and XXX
+#>
+
 $mapping = @{
     'Plain Text' = 'Normal';
     'Selected Text' = 'Visual';
@@ -34,7 +86,7 @@ $mapping = @{
     'Keyword' = 'Keyword';
     'Memory Changed' = '';
     'Number' = 'Number';
-    'Operator' = '';
+    'Operator' = 'Operator';
     'Other Error' = '';
     'Preprocessor Keyword' = '';
     'Refactoring Background' = '';
@@ -45,11 +97,11 @@ $mapping = @{
     'Stale Code' = '';
     'String' = 'String';
     'String(C# @ Verbatim)' = 'String';
-    'User Types' = 'Identifier'; # changed from ''
-    'User Types(Delegates)' = 'Constant'; # changed from ''
-    'User Types(Enums)' = 'Constant'; # changed from ''
-    'User Types(Interfaces)' = 'Constant'; # changed from ''
-    'User Types(Value types)' = 'Constant'; # changed from ''
+    'User Types' = 'Type'; # changed from ''
+    'User Types(Delegates)' = 'Type'; # changed from ''
+    'User Types(Enums)' = 'Type'; # changed from ''
+    'User Types(Interfaces)' = 'Type'; # changed from ''
+    'User Types(Value types)' = 'Type'; # changed from ''
     'XML Attribute' = '';
     'XML Attribute Quotes' = '';
     'XML Attribute Value' = 'String';
@@ -126,10 +178,10 @@ $vim = parseVim $pathToVim
 $colorSchemeName = [System.IO.Path]::GetFileNameWithoutExtension($pathToVim)
 
 pushd '~/Documents/Visual Studio 2008/Settings'
-$xml = [xml] $(get-content ./Exported-2010-02-19.vssettings)
+$xml = [xml] (Get-Content (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Path) 'BaseColorSettings.vssettings'))
 
 # Text Editor guid is {A27B4E24-A735-4D1D-B8E7-9716E1E3D8E0}
-$items = $xml.usersettings.category.category.fontsandcolors.categories.category | 
+$items = $xml.UserSettings.Category.Category.FontsAndColors.Categories.Category | 
     where { $_.Guid -eq '{A27B4E24-A735-4D1D-B8E7-9716E1E3D8E0}' }
 
 $items.GetElementsByTagName('Item') | foreach {
@@ -140,8 +192,6 @@ $items.GetElementsByTagName('Item') | foreach {
         $vimSetting = $vim | where { $_.Name -eq $vimMapping }
 
         if ($vimSetting -ne $null) {
-            # TODO: still need to turn color from RGB to BGR
-
             if ($item.Foreground -ne $Null -and $vimSetting.Foreground -ne $Null) {
                 $item.Foreground = toVsColor $vimSetting.Foreground
             }
@@ -158,4 +208,6 @@ $items.GetElementsByTagName('Item') | foreach {
 $pathToSave = Join-Path $pwd "$colorSchemeName.vssettings"
 
 $xml.Save($pathToSave)
+
+"$pathToSave was created."
 popd
